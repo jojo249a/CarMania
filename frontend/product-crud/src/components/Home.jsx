0bdd79fe90c1301b643e8578ddaf27f28354c74a
jojo249a <jozef.vydra1@gmail.com>
2025-11-25 21:03:03 +0100

    Work on home page

--- frontend/product-crud/src/components/Home.jsx
+++ frontend/product-crud/src/components/Home.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom"
+import { FaArrowRight } from "react-icons/fa"
+import PrimaryLogo from "../assets/primary-logo.svg?react";
+import SecondLogo from "../assets/second-logo.svg?react";
+
+
 import "./Home.css"
 
 export default function Home() {
@@ -7,13 +13,28 @@ export default function Home() {
                 <div className="intro-wrapper">
                     <img src="/showroom6.avif" alt="Background" className="background-img" />
                     <div className="intro-text">
-                        <div class="intro-heading">
+                        <div className="intro-heading">
                             <span>CarMania</span>
+                            <SecondLogo className="second-logo"/>
                         </div>
                         <div className="intro-desc">
-                            <span>All The Cars You Love</span>
+                            <span>All the cars you love</span>
+                        </div>
+                        <div>
+                            <Link className="intro-btn" to="/Shop">
+                                View Offers
+                                <FaArrowRight />
+                            </Link>
                         </div>
                     </div>
+                </div>
+            </section>
+            <section className="offers-section">
+                <div className="offers-wrapper">
+                    <h2 className="offers-heading heading">
+                        <SecondLogo className="second-logo"/>
+                        Our newest offers
+                    </h2>
                 </div>
             </section>
         </div>
--- frontend/product-crud/src/components/Nav.jsx
+++ frontend/product-crud/src/components/Nav.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
+import PrimaryLogo from "../assets/primary-logo.svg?react"
 import "./Nav.css"
 
 export default function Nav() {
     return (
         <nav>
-            <Link className="home-link" to="/"><img src="/logo.svg" alt="Logo" /></Link>
+            <Link className="home-link" to="/"><PrimaryLogo /></Link>
             <ul className="nav-items">
                 <li><Link className="nav-item" to="/shop">Shop</Link></li> 
                 <li><Link className="nav-item" to="/admin">Admin Panel</Link></li>
